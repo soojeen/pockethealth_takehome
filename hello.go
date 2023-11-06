d3b46e4507006a8072c3053c3b74e2bee9a173ab
soojeen <soojeen@gmail.com>
2023-11-05 19:53:14 -0500

    more routes

--- hello.go
+++ hello.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -20,11 +22,19 @@ https://github.com/suyashkumar/dicom
   - return location header with resource URL https://stackoverflow.com/questions/1829875/is-it-ok-by-rest-to-return-content-after-post
   - if client include query param (DICOM Tag), return the header attribute as JSON
 - GET /dicom/:id
+	- use query params to determine type of return? imageType='png', default original dicom?
 */
 
 func main() {
 	r := chi.NewRouter()
 	r.Get("/", rootHandler)
+	r.Route("/dicom-files", func(r chi.Router) {
+		r.Post("/", createDicomFile)
+		r.Route("/{dicomFileID}", func(r chi.Router) {
+			r.Use(dicomFileCtx)
+			r.Get("/", getDicomFile)
+		})
+	})
 
 	// fmt.Println("Hello, World!")
 
@@ -33,4 +43,27 @@ func main() {
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("welcome"))
+}
+
+func createDicomFile(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("TODO create file"))
+}
+
+func dicomFileCtx(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		dicomFileID := chi.URLParam(r, "dicomFileID")
+		// TODO: more details
+		dicomFile := dicomFileID
+		ctx := context.WithValue(r.Context(), "dicomFile", dicomFile)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
+func getDicomFile(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	value, _ := ctx.Value("dicomFile").(string)
+
+	// TODO: what to return??
+	fmt.Println("Hello, World!", value)
+	w.Write([]byte("TODO get file"))
 }
