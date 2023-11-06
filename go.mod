32f7571ab08dc1a3f0f97e2c3eb1958c94b419e4
soojeen <soojeen@gmail.com>
2023-11-05 19:29:01 -0500

    working router

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module pocket-health/server
 
 go 1.21.3
+
+require github.com/go-chi/chi/v5 v5.0.10 // indirect
--- hello.go
+++ hello.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+/*
+# Takehome Notes
+
+https://www.dicomlibrary.com/dicom/dicom-tags/
+https://dicomiseasy.blogspot.com/2011/10/introduction-to-dicom-chapter-1.html
+https://www.digitalocean.com/community/tutorials/how-to-make-an-http-server-in-go#prerequisites
+https://github.com/suyashkumar/dicom
+
+- POST /dicom
+  - extract header attributes
+  - convert to PNG
+  - return location header with resource URL https://stackoverflow.com/questions/1829875/is-it-ok-by-rest-to-return-content-after-post
+  - if client include query param (DICOM Tag), return the header attribute as JSON
+- GET /dicom/:id
+*/
 
 func main() {
-	fmt.Println("Hello, World!")
+	r := chi.NewRouter()
+	r.Get("/", rootHandler)
+
+	// fmt.Println("Hello, World!")
+
+	http.ListenAndServe(":3001", r)
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("welcome"))
 }
